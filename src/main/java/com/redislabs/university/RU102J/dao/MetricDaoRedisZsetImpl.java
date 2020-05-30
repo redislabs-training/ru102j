0c9153c6760f7864fa1dbe13c18aebc7c6080d9e
Simon Prickett <simon@redislabs.com>
2020-05-29 20:54:32 -0700

    Challenge #2 solutions and tests.

--- src/main/java/com/redislabs/university/RU102J/dao/MetricDaoRedisZsetImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/MetricDaoRedisZsetImpl.java
@@ -51,6 +51,12 @@ public class MetricDaoRedisZsetImpl implements MetricDao {
         // START Challenge #2
         String metricKey = RedisSchema.getDayMetricKey(siteId, unit, dateTime);
         Integer minuteOfDay = getMinuteOfDay(dateTime);
+
+        Pipeline pipeline = jedis.pipelined();
+        pipeline.zadd(metricKey, minuteOfDay, new MeasurementMinute(value,
+               minuteOfDay).toString());
+        pipeline.expire(metricKey, METRIC_EXPIRATION_SECONDS);
+        pipeline.sync();
         // END Challenge #2
     }
 
--- src/test/java/com/redislabs/university/RU102J/dao/MetricDaoRedisZsetImplTest.java
+++ src/test/java/com/redislabs/university/RU102J/dao/MetricDaoRedisZsetImplTest.java
@@ -48,14 +48,12 @@ public class MetricDaoRedisZsetImplTest extends JedisDaoTestBase {
     }
 
     // Challenge #2
-    @Ignore
     @Test
     public void testSmall() {
         testInsertAndRetrieve(1);
     }
 
     // Challenge #2
-    @Ignore
     @Test
     public void testOneDay() {
         testInsertAndRetrieve(60 * 24);
@@ -63,7 +61,6 @@ public class MetricDaoRedisZsetImplTest extends JedisDaoTestBase {
 
 
     // Challenge #2
-    @Ignore
     @Test
     public void testMultipleDays() {
         testInsertAndRetrieve(60 * 70);
