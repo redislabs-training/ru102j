955f23e015eb17e5e0cb692fe38f34c13459e587
Simon Prickett <simon@redislabs.com>
2020-05-29 20:59:01 -0700

    Adds Challenge #3 solution.

--- src/main/java/com/redislabs/university/RU102J/dao/SiteStatsDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/SiteStatsDaoRedisImpl.java
@@ -47,7 +47,8 @@ public class SiteStatsDaoRedisImpl implements SiteStatsDao {
             ZonedDateTime day = reading.getDateTime();
             String key = RedisSchema.getSiteStatsKey(siteId, day);
 
-            updateBasic(jedis, key, reading);
+            //updateBasic(jedis, key, reading);
+            updateOptimized(jedis, key, reading);
         }
     }
 
@@ -81,6 +82,20 @@ public class SiteStatsDaoRedisImpl implements SiteStatsDao {
     // Challenge #3
     private void updateOptimized(Jedis jedis, String key, MeterReading reading) {
         // START Challenge #3
+        try (Transaction t = jedis.multi()) {
+            String reportingTime = ZonedDateTime.now(ZoneOffset.UTC).toString();
+            t.hset(key, SiteStats.reportingTimeField, reportingTime);
+            t.hincrBy(key, SiteStats.countField, 1);
+            t.expire(key, weekSeconds);
+            this.compareAndUpdateScript.updateIfGreater(t, key, SiteStats.maxWhField,
+                    reading.getWhGenerated());
+            this.compareAndUpdateScript.updateIfLess(t, key, SiteStats.minWhField,
+                    reading.getWhGenerated());
+            this.compareAndUpdateScript.updateIfGreater(t, key, SiteStats.maxCapacityField,
+                    getCurrentCapacity(reading));
+
+            t.exec();
+        }        
         // END Challenge #3
     }
 
